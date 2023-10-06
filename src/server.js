f9e89fbbd9e52ad78e74259e6c450596043e32d6
DucHaiDev <duchaidev@gmail.com>
2023-10-07 01:39:00 +0700

    Update

---
+++ src/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const connectDB = require("./config/connectDB");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 6969;
+
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", process.env.URL_REACT);
+
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
